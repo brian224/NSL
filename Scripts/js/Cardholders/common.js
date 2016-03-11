fb2acfc4a263b4f698dcb36601cb4d200563ceca
Brian Lin <brianlin224@gmail.com>
2016-03-11 17:41:54 +0800

    files backup

--- Scripts/js/Cardholders/common.js
+++ Scripts/js/Cardholders/common.js
@@ -1,26 +1,37 @@
 (function (window, document, jQuery, undefined) {
 	'use strict';
 
-	// if ( Projects.Factory.Device.test(navigator.userAgent) ) {
-	// } else {
-	// }
-
 	$('.jq-menu').on('click' , function(e){
 		e.preventDefault();
 		Projects.Factory.Menu.Click(e, this);
 	});
 
-	$('.jq-start').on('click' , function(e){
+	$('.jq-lazybag').on('click' , function(e){
 		e.preventDefault();
-		Projects.Factory.Start.Click();
+		Projects.Factory.LazyBag.Click(e, this);
 	});
 
-	$('.jq-play').on('click' , function(e){
+	$('.jq-start').on('click' , function(e){
 		e.preventDefault();
-		Projects.Factory.FB.GetLoaginState();
+		Projects.Factory.Start.Click(this);
+	});
+
+	$('.jq-complete').on('click' , function(){
+		localStorage.setItem('lazybag', 'done');
+	});
+
+	$(window).load(function(e){
+		$('.jq-play').on('click' , function(e){
+			if (!$(this).hasClass('disable')) {
+				e.preventDefault();
+				Projects.Factory.FB.GetLoaginState();
+			}
+		});
 	});
 
 	$(document).ready(function(e){
+		Projects.Factory.Menu.Init();
 		Projects.Factory.FB.Init();
+		Projects.Factory.GetUserAgent();
 	});
 }(window, document, $));
