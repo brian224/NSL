b39627cdc4f84ecb268b57f3485812b0d8e3f844
Brian Lin <brianlin224@gmail.com>
2016-05-24 19:48:15 +0800

    demo complete

--- Scripts/js/common.js
+++ Scripts/js/common.js
@@ -6,6 +6,24 @@
 	function index() {
 	}
 
-	projects.$w.load(function(){});
+	projects.$w.load(function(){
+		$('.btn-check').on('click', function(){
+			var $another = $(this).parent().siblings().find('.btn-check');
+
+			if ($(this).hasClass('is-checked')) {
+				$(this).removeClass('is-checked').addClass('ani-reverse');
+			} else {
+				$(this).addClass('is-checked').removeClass('ani-reverse')
+			}
+
+			if ($another.hasClass('is-checked')) {
+				$another.removeClass('is-checked').addClass('ani-reverse');
+			}
+		});
+		
+		$('.image-wrap').on('webkitAnimationEnd oAnimationend oAnimationEnd msAnimationEnd animationend', function(){
+			$(this).siblings().removeClass('ani-reverse');
+		});
+	});
 	projects.$d.ready(function(){});
 }(window, document, $));
