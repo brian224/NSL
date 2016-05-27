73f998e6f69262e1721df5902a60b76b9b9b3cc7
Brian Lin <brianlin224@gmail.com>
2016-05-27 19:25:56 +0800

    daily commit

--- Scripts/js/common.js
+++ Scripts/js/common.js
@@ -4,38 +4,50 @@
 	var common = new index();
 
 	function index() {
+		this._start    = '.jq-start';
+		this._required = '.jq-required';
+		this._lContent = '.l-content';
+		this._checkbox = '.jq-checkbox';
 	}
 
 	projects.$w.load(function(){
-		$('.jq-start').on('click', function(){
-			if ($('.btn-check').hasClass('is-checked')) {
-				$('.l-main').removeClass('is-index').addClass('is-quest');
-			} else {
-				$('.btn-check').addClass('btn-shake');
+		if ($(common._lContent).hasClass('index')) {
+			$(common._start).on('click', function(){
+				if ($(common._required).hasClass('is-checked')) {
+					$('.l-main').removeClass('is-index').addClass('is-quest');
+				} else {
+					$(common._required).addClass('btn-shake');
 
-				$('.btn-check').on('webkitAnimationEnd oAnimationend oAnimationEnd msAnimationEnd animationend', function(){
-					$(this).removeClass('btn-shake');
-				});
-			}
-		});
+					$(common._required).on('webkitAnimationEnd oAnimationend oAnimationEnd msAnimationEnd animationend', function(){
+						$(this).removeClass('btn-shake');
+					});
+				}
+			});
 
-		$('.btn-check').on('click', function(){
-			var $another = $(this).parent().siblings().find('.btn-check');
+			$(common._required).on('click', function(){
+				$(this).toggleClass('is-checked');
+			});
+		}
 
-			if ($(this).hasClass('is-checked')) {
-				$(this).removeClass('is-checked').addClass('ani-reverse');
-			} else {
-				$(this).addClass('is-checked').removeClass('ani-reverse')
-			}
+		if ($(common._lContent).hasClass('quest-1')) {
+			$(common._checkbox).on('click', function(){
+				var $another = $(this).parent().siblings().find(common._checkbox);
 
-			if ($another.hasClass('is-checked')) {
-				$another.removeClass('is-checked').addClass('ani-reverse');
-			}
-		});
+				if ($(this).hasClass('is-checked')) {
+					$(this).removeClass('is-checked').addClass('ani-reverse');
+				} else {
+					$(this).addClass('is-checked').removeClass('ani-reverse')
+				}
 
-		$('.image-wrap').on('webkitAnimationEnd oAnimationend oAnimationEnd msAnimationEnd animationend', function(){
-			$(this).siblings().removeClass('ani-reverse');
-		});
+				if ($another.hasClass('is-checked')) {
+					$another.removeClass('is-checked').addClass('ani-reverse');
+				}
+			});
+
+			$('.image-wrap').on('webkitAnimationEnd oAnimationend oAnimationEnd msAnimationEnd animationend', function(){
+				$(this).siblings().removeClass('ani-reverse');
+			});
+		}
 	});
 	projects.$d.ready(function(){});
 }(window, document, $));
