64751f4a5123a6ebda8d1b051cc3595d87c27e5d
Brian Lin <brianlin224@gmail.com>
2016-05-31 19:17:21 +0800

    stock in animation

--- Scripts/js/common.js
+++ Scripts/js/common.js
@@ -16,82 +16,98 @@
 			{
 				'number' : '1',
 				'title' : '你的性別？',
-				'selection' : ['女', '男']
+				'selection' : ['女', '男'],
+				'selectMeta' : ['girl', 'boy']
 			},
 			{
 				'number' : '2',
 				'title' : '你婚了嗎？',
-				'selection' : ['單身', '已婚']
+				'selection' : ['單身', '已婚'],
+				'selectMeta' : ['single', 'merryed']
 			},
 			{
 				'number' : '3',
 				'title' : '您的年齡？',
-				'selection' : ['', '']
+				'selection' : ['', ''],
+				'selectMeta' : ['', '']
 			},
 			{
 				'number' : '4',
 				'title' : '你有小孩嗎？',
-				'selection' : ['', '']
+				'selection' : ['', ''],
+				'selectMeta' : ['', '']
 			},
 			{
 				'number' : '5',
 				'title' : '你正處於哪個人生階段？',
-				'selection' : ['', '']
+				'selection' : ['', ''],
+				'selectMeta' : ['', '']
 			},
 			{
 				'number' : '6',
 				'title' : '不好意思，你的年收入？',
-				'selection' : ['', '']
+				'selection' : ['', ''],
+				'selectMeta' : ['', '']
 			},
 			{
 				'number' : '7',
 				'title' : '日常支出支多少',
-				'selection' : ['', '']
+				'selection' : ['', ''],
+				'selectMeta' : ['', '']
 			},
 			{
 				'number' : '8',
 				'title' : '誰都不想破病',
-				'selection' : ['', '']
+				'selection' : ['', ''],
+				'selectMeta' : ['', '']
 			},
 			{
 				'number' : '9',
 				'title' : '醫療項目保障額度',
-				'selection' : ['', '']
+				'selection' : ['', ''],
+				'selectMeta' : ['', '']
 			},
 			{
 				'number' : '10',
 				'title' : '夢想三擇一',
-				'selection' : ['', '']
+				'selection' : ['', ''],
+				'selectMeta' : ['', '']
 			},
 			{
 				'number' : '11',
 				'title' : '無痛退休',
-				'selection' : ['', '']
+				'selection' : ['', ''],
+				'selectMeta' : ['', '']
 			},
 			{
 				'number' : '12',
 				'title' : '給兒女無憂的求學時光',
-				'selection' : ['', '']
+				'selection' : ['', ''],
+				'selectMeta' : ['', '']
 			},
 			{
 				'number' : '13',
 				'title' : '',
-				'selection' : ['', '']
+				'selection' : ['', ''],
+				'selectMeta' : ['', '']
 			},
 			{
 				'number' : '14',
 				'title' : '',
-				'selection' : ['', '']
+				'selection' : ['', ''],
+				'selectMeta' : ['', '']
 			},
 			{
 				'number' : '15',
 				'title' : '',
-				'selection' : ['', '']
+				'selection' : ['', ''],
+				'selectMeta' : ['', '']
 			},
 			{
 				'number' : '16',
 				'title' : '',
-				'selection' : ['', '']
+				'selection' : ['', ''],
+				'selectMeta' : ['', '']
 			}
 		];
 	}
@@ -122,7 +138,7 @@
 				if ($(this).hasClass('is-checked')) {
 					$(this).removeClass('is-checked').addClass('ani-reverse');
 				} else {
-					$(this).addClass('is-checked').removeClass('ani-reverse')
+					$(this).addClass('is-checked').removeClass('ani-reverse');
 				}
 
 				if ($another.hasClass('is-checked')) {
@@ -135,28 +151,55 @@
 			});
 		}
 
+		if ($(common._lContent).hasClass('quest-2')) {
+			$(common._checkbox).on('click', function(){
+				var _total = 0;
+
+				var $another = $(this).parent().siblings().find(common._checkbox);
+
+				if ($(this).hasClass('is-checked')) {
+					$(this).removeClass('is-checked');
+					$(common._checkbox).addClass('ani-reverse');
+					$(common._transition).attr('data-select', '');
+				} else {
+					$(this).addClass('is-checked');
+					$(common._checkbox).removeClass('ani-reverse');
+					$(common._transition).attr('data-select', $(this).data('meta'));
+				}
+
+				if ($another.hasClass('is-checked')) {
+					$(common._checkbox).removeClass('ani-reverse');
+				}
+			});
+
+			$(common._imageWrap).on('webkitAnimationEnd oAnimationend oAnimationEnd msAnimationEnd animationend', function(){
+				$(common._checkbox).removeClass('ani-reverse');
+			});
+		}
+
 		$(common._btnTopic).on('click', function(){
 			var $quest   = $(common._lContent + '.quest'),
 				_num     = $quest.data('quest'),
-				_gender  = $quest.find('.is-checked').data('gender'),
+				_meta    = $quest.find('.is-checked').data('meta'),
 				_compare = '';
 
 			if (_num === 1 && $(this).hasClass('btn-next')) {
-				if (_gender !== undefined) {
-					$(common._transition).addClass('chosen-' + _gender);
+				if (_meta !== undefined) {
+					$(common._transition).addClass('chosen-' + _meta);
 
 					$(common._questTitle).on('webkitTransitionEnd oTransitionend oTransitionEnd msTransitionEnd transitionend', function(){
 						$(this).find('em').text(common._exam[_num].title);
 
 						for (var i = 0; i < common._exam[_num].selection.length; i++) {
+							$(common._transition + ' .list').eq(i).find('.btn-check').attr('data-meta', common._exam[_num].selectMeta[i]);
 							$(common._transition + ' .list').eq(i).find('em').text(common._exam[_num].selection[i]);
 						};
 					});
 				}
 
-				if (_gender === 'boy') {
+				if (_meta === 'boy') {
 					_compare = 'girl';
-				} else if (_gender === 'girl') {
+				} else if (_meta === 'girl') {
 					_compare = 'boy';
 				}
 			} else {
