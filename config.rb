df21b300f3411bc5a7e0a6feed340869d81aa7a3
Brian Lin <brianlin224@gmail.com>
2016-07-15 10:34:47 +0800

    change file pass

--- config.rb
+++ config.rb
@@ -3,10 +3,10 @@
 
 # Set this to the root of your project when deployed:
 http_path       = "/"
-css_dir         = "Content/css"
+css_dir         = "_ui/desktop/nslcp_common/js/needanalysis/Content/css"
 sass_dir        = "sass"
-images_dir      = "Content/img"
-javascripts_dir = "Scripts/js"
+images_dir      = "_ui/desktop/nslcp_common/js/needanalysis/Content/img"
+javascripts_dir = "_ui/desktop/nslcp_common/js/needanalysis/Scripts/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
